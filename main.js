12db86b35cb099f2708df00d4bbffa888ca6075a
alimurad4555 <alimurad4555gmail.com>
2024-06-19 02:47:02 -0700

    ;tuple

---
+++ main.js
@@ -0,0 +1,7 @@
+// tuple 
+var array = ['sallahuden', 1, true];
+array.push('hello');
+// array
+var arr = [1, 2, 3,];
+arr.push(4);
+// tubles our array faraq check kara
---
+++ main.ts
@@ -0,0 +1,20 @@
+// tuple 
+
+type tuple = readonly [string,number,boolean];
+
+let array : tuple =['sallahuden',1,true];
+
+array.push('hello')
+
+
+
+// array
+
+
+let arr =[1,2,3,];
+
+arr.push(4);
+
+
+
+// tubles our array faraq check kara
